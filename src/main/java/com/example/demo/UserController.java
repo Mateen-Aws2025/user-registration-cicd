beff3637cf5301d807af31fd62282c92e0a1fe33
Mateen-Aws2025 <mateen.aws2025@gmail.com>
2026-02-28 06:34:59 +0530

    Initial commit - Spring Boot User Registration with H2

---
+++ src/main/java/com/example/demo/UserController.java
@@ -0,0 +1,25 @@
+
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping
+    public User registerUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
